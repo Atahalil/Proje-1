24876b3bc64bf588df54b0a184cdcd7ebba3aa49
Atahalil <167443821+Atahalil@users.noreply.github.com>
2025-02-25 19:47:00 +0300

    Add files via upload

--- proje 1.py
+++ proje 1.py
@@ -1,3 +1,4 @@
+#1. Proje
 def flatten(lst):
     flat_list = []
     for item in lst:
@@ -10,4 +11,19 @@ def flatten(lst):
 # Örnek kullanım
 input_list = [[1,'a',['cat'],2],[[[3]],'dog'],4,5]
 output_list = flatten(input_list)
+print(output_list)
+
+#2. Proje
+def reverse_elements(lst):
+    reversed_list = []
+    for item in lst:
+        if isinstance(item, list):
+            reversed_list.append(reverse_elements(item)[::-1])
+        else:
+            reversed_list.append(item)
+    return reversed_list
+
+# Örnek kullanım
+input_list = [[1, 2], [3, 4], [5, 6, 7]]
+output_list = reverse_elements(input_list[::-1])
 print(output_list)
